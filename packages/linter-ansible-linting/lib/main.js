6c320c6cb7d592ccec93ad86f63644caeedb4329
samuel <quenea.samuel@gmail.com>
2017-08-30 14:44:38 +0200

    first commit

---
+++ packages/linter-ansible-linting/lib/main.js
@@ -0,0 +1,281 @@
+'use babel';
+
+export default {
+  config: {
+    ansibleLintExecutablePath: {
+      title: 'Ansible-Lint Executable Path',
+      type: 'string',
+      description: 'Path to Ansible-Lint executable (e.g. /usr/bin/ansible-lint) if not in shell env path.',
+      default: 'ansible-lint',
+    },
+    useRulesDirs: {
+      title: 'Use non-default rules directories with Ansible-Lint.',
+      type: 'boolean',
+      default: false,
+    },
+    rulesDirDefault: {
+      title: 'Additionally use the default rules directories with Ansible-Lint (only if use non-default rules directories checked).',
+      type: 'boolean',
+      default: false,
+    },
+    rulesDirs: {
+      title: 'Rules Directories',
+      type: 'array',
+      description: 'Non-default rules directories to use with Ansible-Lint. (only if use non-default rules directories checked)',
+      default: ['/usr/local/lib/python2.7/site-packages/ansiblelint/rules', '/usr/local/lib/python2.7/dist-packages/ansiblelint/rules'],
+      items: {
+        type: 'string'
+      }
+    },
+    excludeDirs: {
+      title: 'Exclude Directories',
+      type: 'array',
+      description: 'Absolute path directories to exclude during linting.',
+      default: [''],
+      items: {
+        type: 'string'
+      }
+    },
+    ANSIBLE0002: {
+      title: 'Skip trailing whitespace.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0004: {
+      title: 'Skip Git checkouts must contain explicit version.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0005: {
+      title: 'Skip Mercurial checkouts must contain explicit revision.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0006: {
+      title: 'Skip using command rather than module.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0007: {
+      title: 'Skip using command rather than an argument to e.g. file.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0008: {
+      title: 'Skip deprecated sudo.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0009: {
+      title: 'Skip octal file permissions must contain leading zero.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0010: {
+      title: 'Skip package installs should not use latest.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0011: {
+      title: 'Skip all tasks should be named.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0012: {
+      title: 'Skip commands should not change things if nothing needs doing.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0013: {
+      title: 'Skip use shell only when shell functionality is required.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0014: {
+      title: 'Skip environment variables don\'t work as part of command.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0015: {
+      title: 'Skip using bare variables is deprecated.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0016: {
+      title: 'Skip tasks that run when changed should likely be handlers.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0017: {
+      title: 'Skip become_user requires become to work as expected.',
+      type: 'boolean',
+      default: false,
+    },
+    ANSIBLE0018: {
+      title: 'Skip deprecated always_run.',
+      type: 'boolean',
+      default: false,
+    }
+  },
+
+  // activate linter
+  activate() {
+    const helpers = require("atom-linter");
+
+    // check for ansible-lint >= minimum version
+    helpers.exec(atom.config.get('linter-ansible-linting.ansibleLintExecutablePath'), ['--version']).then(output => {
+      const version = /ansible-lint (.*)/.exec(output)[1];
+
+      if (parseFloat(version) < 2.5) {
+        atom.notifications.addError(
+          'The ansible-lint installed in your path is too old to support standard linter-ansible-linting features.',
+          {
+            detail: "Please upgrade your version of ansible-lint to >= 2.5.\n"
+          }
+        );
+      }
+    });
+  },
+
+  provideLinter() {
+    return {
+      name: 'Ansible-Lint',
+      grammarScopes: ['source.ansible'],
+      scope: 'file',
+      lintsOnChange: false,
+      lint: (activeEditor) => {
+        // setup variables
+        const helpers = require('atom-linter');
+        const lint_regex = /(.*):(\d+).*ANSIBLE\d{4}\]\s(.*)/;
+        const path = require('path');
+        const file = activeEditor.getPath();
+        const correct_file = new RegExp(file);
+
+        // pep8 output
+        var args = ['-p']
+
+        // generate array of possible skips
+        var skips = []
+        for (var skip in atom.config.getAll('linter-ansible-linting')[0].value)
+          if (/^ANSIBLE/.exec(skip))
+            skips.push(skip)
+
+        // this doesn't work for some reason
+        // var skips = atom.config.getAll('linter-ansible-linting')[0].value.filter(function(skip) {
+        //   return /^ANSIBLE/.test(skip);
+        // });
+
+        // skip checks that user has opted to skip
+        var skipList = ''
+
+        for (i = 0; i < skips.length; i++)
+          if (atom.config.get('linter-ansible-linting.' + skips[i]))
+            skipList += skips[i] + ',';
+
+        if (skipList != '')
+          args = args.concat(['-x', skipList])
+
+        // add additional rules directories
+        if (atom.config.get('linter-ansible-linting.useRulesDirs')) {
+          if (atom.config.get('linter-ansible-linting.rulesDirDefault'))
+            args = args.concat(['-R'])
+
+          for (i = 0; i < atom.config.get('linter-ansible-linting.rulesDirs').length; i++)
+            args = args.concat(['-r', atom.config.get('linter-ansible-linting.rulesDirs')[i]]);
+        }
+
+        // exclude certain directories from checks
+        if (atom.config.get('linter-ansible-linting.excludeDirs')[0] != '')
+          for (i = 0; i < atom.config.get('linter-ansible-linting.excludeDirs').length; i++)
+            args = args.concat(['--exclude', atom.config.get('linter-ansible-linting.excludeDirs')[i]]);
+
+        // add file to check
+        args.push(file);
+
+        // initialize variable for linter return here for either linter output or errors
+        var toReturn = [];
+
+        return helpers.exec(atom.config.get('linter-ansible-linting.ansibleLintExecutablePath'), args, {cwd: path.dirname(file), ignoreExitCode: true}).then(output => {
+          output.split(/\r?\n/).forEach(function (line) {
+            const lint_matches = lint_regex.exec(line);
+            const correct_file_matches = correct_file.exec(line);
+
+            // check for normal linter checks output
+            if (lint_matches != null && correct_file_matches != null) {
+              toReturn.push({
+                severity: 'warning',
+                excerpt: lint_matches[3],
+                location: {
+                  file: file,
+                  position: [[Number.parseInt(lint_matches[2]) - 1, 0], [Number.parseInt(lint_matches[2]) - 1, 1]],
+                },
+              });
+            }
+            // check for linting issues in other files
+            else if (lint_matches != null) {
+              toReturn.push({
+                severity: 'warning',
+                excerpt: lint_matches[3],
+                location: {
+                  file: lint_matches[1],
+                  position: [[Number.parseInt(lint_matches[2]) - 1, 0], [Number.parseInt(lint_matches[2]) - 1, 1]],
+                },
+              });
+            }
+          });
+          return toReturn;
+        })
+        .catch(error => {
+          // check for unusual issues with playbook files
+          const missing_file_matches = /WARNING: Couldn't open (.*) - No such file or directory/.exec(error.message);
+          const unreadable_file_matches = /the file_name (.*) does not exist, or is not readable/.exec(error.message);
+          const syntax_matches = /(?:raise Ansible(Parser)?Error|Syntax Error while loading YAML|Couldn't parse task at|AttributeError)/.exec(error.message);
+
+          // check for missing file or directory
+          if (missing_file_matches != null) {
+            toReturn.push({
+              severity: 'error',
+              excerpt: 'Missing file ' + missing_file_matches[1] + '. Please fix before continuing linter use.',
+              location: {
+                file: file,
+                position: [[0, 0], [0, 1]],
+              },
+            });
+          }
+          // check for unreadable file
+          else if (unreadable_file_matches != null) {
+            toReturn.push({
+              severity: 'error',
+              excerpt: 'Unreadable or not file ' + unreadable_file_matches[1] + '. Please fix before continuing linter use.',
+              location: {
+                file: file,
+                position: [[0, 0], [0, 1]],
+              },
+            });
+          }
+          // check for syntax issue
+          else if (syntax_matches != null) {
+            toReturn.push({
+              severity: 'error',
+              excerpt: 'This file, an include, or role, has a syntax error. Please fix before continuing linter use.',
+              location: {
+                file: file,
+                position: [[0, 0], [0, 1]],
+              },
+            });
+          }
+          // output other errors directly to Atom notification display
+          else {
+            atom.notifications.addError(
+              'An unexpected error with ansible, ansible-lint, linter-ansible-linting, atom, linter, and/or your playbook, has occurred.',
+              {
+                detail: error.message
+              }
+            );
+          };
+          return toReturn;
+        });
+      }
+    };
+  }
+};
---
+++ packages/linter-ansible-linting/spec/linter-ansible-linting-spec.js
@@ -0,0 +1,336 @@
+'use babel';
+
+import * as path from 'path';
+
+describe('The Ansible Lint provider for Linter', () => {
+  const lint = require(path.join('..', 'lib', 'main.js')).provideLinter().lint;
+
+  beforeEach(() => {
+    atom.workspace.destroyActivePaneItem();
+    waitsForPromise(() => {
+      atom.packages.activatePackage('linter-ansible-linting');
+      return atom.packages.activatePackage('language-ansible').then(() =>
+        atom.workspace.open(path.join(__dirname, 'fixtures', 'clean.yml'))
+      );
+    });
+  });
+
+  describe('checks a file with multiple issues and', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures', 'normal_checks.yml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds at least one message', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toBeGreaterThan(0);
+        })
+      );
+    });
+
+    it('verifies the messages', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].type).toBeDefined();
+          expect(messages[0].type).toEqual('Warning');
+          expect(messages[0].text).toBeDefined();
+          expect(messages[0].text).toEqual('Package installs should not use latest');
+          expect(messages[0].filePath).toBeDefined();
+          expect(messages[0].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[0].range).toBeDefined();
+          expect(messages[0].range.length).toBeDefined();
+          expect(messages[0].range.length).toEqual(2);
+          expect(messages[0].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[1].type).toBeDefined();
+          expect(messages[1].type).toEqual('Warning');
+          expect(messages[1].text).toBeDefined();
+          expect(messages[1].text).toEqual('Use shell only when shell functionality is required');
+          expect(messages[1].filePath).toBeDefined();
+          expect(messages[1].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[1].range).toBeDefined();
+          expect(messages[1].range.length).toBeDefined();
+          expect(messages[1].range.length).toEqual(2);
+          expect(messages[1].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[2].type).toBeDefined();
+          expect(messages[2].type).toEqual('Warning');
+          expect(messages[2].text).toBeDefined();
+          expect(messages[2].text).toEqual('Octal file permissions must contain leading zero');
+          expect(messages[2].filePath).toBeDefined();
+          expect(messages[2].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[2].range).toBeDefined();
+          expect(messages[2].range.length).toBeDefined();
+          expect(messages[2].range.length).toEqual(2);
+          expect(messages[2].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[3].type).toBeDefined();
+          expect(messages[3].type).toEqual('Warning');
+          expect(messages[3].text).toBeDefined();
+          expect(messages[3].text).toEqual('All tasks should be named');
+          expect(messages[3].filePath).toBeDefined();
+          expect(messages[3].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[3].range).toBeDefined();
+          expect(messages[3].range.length).toBeDefined();
+          expect(messages[3].range.length).toEqual(2);
+          expect(messages[3].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[4].type).toBeDefined();
+          expect(messages[4].type).toEqual('Warning');
+          expect(messages[4].text).toBeDefined();
+          expect(messages[4].text).toEqual('Commands should not change things if nothing needs doing');
+          expect(messages[4].filePath).toBeDefined();
+          expect(messages[4].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[4].range).toBeDefined();
+          expect(messages[4].range.length).toBeDefined();
+          expect(messages[4].range.length).toEqual(2);
+          expect(messages[4].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[5].type).toBeDefined();
+          expect(messages[5].type).toEqual('Warning');
+          expect(messages[5].text).toBeDefined();
+          expect(messages[5].text).toEqual('git used in place of git module');
+          expect(messages[5].filePath).toBeDefined();
+          expect(messages[5].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[5].range).toBeDefined();
+          expect(messages[5].range.length).toBeDefined();
+          expect(messages[5].range.length).toEqual(2);
+          expect(messages[5].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[6].type).toBeDefined();
+          expect(messages[6].type).toEqual('Warning');
+          expect(messages[6].text).toBeDefined();
+          expect(messages[6].text).toEqual('hg used in place of hg module');
+          expect(messages[7].filePath).toBeDefined();
+          expect(messages[7].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[7].range).toBeDefined();
+          expect(messages[7].range.length).toBeDefined();
+          expect(messages[7].range.length).toEqual(2);
+          expect(messages[7].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[7].type).toBeDefined();
+          expect(messages[7].type).toEqual('Warning');
+          expect(messages[7].text).toBeDefined();
+          expect(messages[7].text).toEqual("Found a bare variable 'items' used in a 'with_items' loop. You should use the full variable syntax ('{{items}}')");
+          expect(messages[8].filePath).toBeDefined();
+          expect(messages[8].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[8].range).toBeDefined();
+          expect(messages[8].range.length).toBeDefined();
+          expect(messages[8].range.length).toEqual(2);
+          expect(messages[8].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[8].type).toBeDefined();
+          expect(messages[8].type).toEqual('Warning');
+          expect(messages[8].text).toBeDefined();
+          expect(messages[8].text).toEqual('Git checkouts must contain explicit version');
+          expect(messages[9].filePath).toBeDefined();
+          expect(messages[9].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[9].range).toBeDefined();
+          expect(messages[9].range.length).toBeDefined();
+          expect(messages[9].range.length).toEqual(2);
+          expect(messages[9].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[9].type).toBeDefined();
+          expect(messages[9].type).toEqual('Warning');
+          expect(messages[9].text).toBeDefined();
+          expect(messages[9].text).toEqual('Environment variables don\'t work as part of command');
+          expect(messages[10].filePath).toBeDefined();
+          expect(messages[10].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[10].range).toBeDefined();
+          expect(messages[10].range.length).toBeDefined();
+          expect(messages[10].range.length).toEqual(2);
+          expect(messages[10].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[10].type).toBeDefined();
+          expect(messages[10].type).toEqual('Warning');
+          expect(messages[11].text).toBeDefined();
+          expect(messages[11].text).toEqual('deprecated sudo_user feature');
+          expect(messages[11].filePath).toBeDefined();
+          expect(messages[11].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[11].range).toBeDefined();
+          expect(messages[11].range.length).toBeDefined();
+          expect(messages[11].range.length).toEqual(2);
+          expect(messages[11].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[12].text).toBeDefined();
+          expect(messages[12].text).toEqual('Tasks that run when changed should likely be handlers');
+          expect(messages[12].filePath).toBeDefined();
+          expect(messages[12].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[12].range).toBeDefined();
+          expect(messages[12].range.length).toBeDefined();
+          expect(messages[12].range.length).toEqual(2);
+          expect(messages[12].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[13].text).toBeDefined();
+          expect(messages[13].text).toEqual('become_user requires become to work as expected');
+          expect(messages[13].filePath).toBeDefined();
+          expect(messages[13].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[13].range).toBeDefined();
+          expect(messages[13].range.length).toBeDefined();
+          expect(messages[13].range.length).toEqual(2);
+          expect(messages[13].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[14].text).toBeDefined();
+          expect(messages[14].text).toEqual('Deprecated always_run');
+          expect(messages[14].filePath).toBeDefined();
+          expect(messages[14].filePath).toMatch(/.+normal_checks\.yml$/);
+          expect(messages[14].range).toBeDefined();
+          expect(messages[14].range.length).toBeDefined();
+          expect(messages[14].range.length).toEqual(2);
+          expect(messages[14].range).toEqual([[0, 0], [0, 32]]);
+        });
+      });
+    });
+  });
+
+  describe('checks a file that would throw an error and', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures', 'syntax.yml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds at least one message', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toBeGreaterThan(0);
+        })
+      );
+    });
+
+    it('verifies the messages', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].type).toBeDefined();
+          expect(messages[0].type).toEqual('Error');
+          expect(messages[0].text).toBeDefined();
+          expect(messages[0].text).toEqual('This file, an include, or role has a syntax error. Please fix before continuing linter use.');
+          expect(messages[0].filePath).toBeDefined();
+          expect(messages[0].filePath).toMatch(/.+syntax\.yml$/);
+        });
+      });
+    });
+  });
+
+  describe('checks a file that would throw an error and', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures', 'missing_include.yml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds at least one message', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toBeGreaterThan(0);
+        })
+      );
+    });
+
+    it('verifies the messages', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].type).toBeDefined();
+          expect(messages[0].type).toEqual('Error');
+          expect(messages[0].text).toBeDefined();
+          expect(messages[0].text).toMatch(/Missing file/);
+          expect(messages[0].filePath).toBeDefined();
+          expect(messages[0].filePath).toMatch(/.+missing_include\.yml$/);
+        });
+      });
+    });
+  });
+
+  describe('checks a file that would throw an error and', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures', 'unreadable_file.yml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds at least one message', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toBeGreaterThan(0);
+        })
+      );
+    });
+
+    it('verifies the message', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].type).toBeDefined();
+          expect(messages[0].type).toEqual('Error');
+          expect(messages[0].text).toBeDefined();
+          expect(messages[0].text).toMatch(/Unreadable or not file/);
+          expect(messages[0].filePath).toBeDefined();
+          expect(messages[0].filePath).toMatch(/.+unreadable_file\.yml$/);
+        });
+      });
+    });
+  });
+
+  describe('checks a file that has an out bounds warning in an include and', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures', 'include_has_issues.yml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds one message', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(1);
+        })
+      );
+    });
+
+    it('verifies the message in another file', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[9].filePath).toBeDefined();
+          expect(messages[9].filePath).toMatch(/.+include_with_issues\.yml$/);
+          expect(messages[9].range).toBeDefined();
+          expect(messages[9].range.length).toBeDefined();
+          expect(messages[9].range.length).toEqual(2);
+          expect(messages[9].range).toEqual([[0, 0], [0, 32]]);
+          expect(messages[9].type).toBeDefined();
+          expect(messages[9].type).toEqual('Warning');
+          expect(messages[9].text).toBeDefined();
+          expect(messages[9].text).toEqual('Environment variables don\'t work as part of command');
+        });
+      });
+    });
+  });
+
+  it('finds nothing wrong with a valid file', () => {
+    waitsForPromise(() => {
+      const goodFile = path.join(__dirname, 'fixtures', 'clean.yml');
+      return atom.workspace.open(goodFile).then(editor =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(0);
+        })
+      );
+    });
+  });
+
+  it('ignores an included file', () => {
+    waitsForPromise(() => {
+      const goodFile = path.join(__dirname, 'fixtures', 'issues.yml');
+      return atom.workspace.open(goodFile).then(editor =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(0);
+        })
+      );
+    });
+  });
+});
